d373803dee5e93ff6610551e097c62ddcb9e9f63
MOUSIGAN <mousigan12@gmail.com>
2026-06-08 11:20:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+
+# Fixed: Added quotes around string
 st.title("my live application")
+
 with st.sidebar:
-data = option_menu(
-  menu_title = "my apps",
-  option=["home","About","Services"],
-)
+    # Fixed: Added quotes and changed 'option' to 'options'
+    data = option_menu(
+        menu_title="my apps",
+        options=["home", "About", "Services"],
+    )
